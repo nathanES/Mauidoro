615ff6425e472d6ecae5518c505b887583752d0a
nathan <nathan.esslinger67@gmail.com>
2023-09-22 22:21:57 +0200

    Tentative d'ajout des compteurs de pomodoro

--- Mauidoro/Model/TaskTodo.cs
+++ Mauidoro/Model/TaskTodo.cs
@@ -1,9 +1,17 @@
 namespace Mauidoro.Model;
 
-public class Task
+public class TaskTodo
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public int NbrPomodoro { get; set; }
+    public string? Description { get; set; }
     public DateTime? PlannedDate { get; set; }
+
+    private static int iId = 0;
+    public TaskTodo()
+    {
+        Id = iId;
+        iId++;
+    }
 }
--- Mauidoro/ViewModel/MainViewModel.cs
+++ Mauidoro/ViewModel/MainViewModel.cs
@@ -2,59 +2,138 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Mauidoro.Controls;
+using Mauidoro.Model;
 
 namespace Mauidoro.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
-    private IConnectivity connectivity;
     public MainViewModel(IConnectivity connectivity)
     {
         TimerView.TimerFocusFinishedEvent += TimerFinished;
-        items = new ObservableCollection<string>();
-        this.connectivity = connectivity;
+        taskTodoList = new ObservableCollection<TaskTodo>();
     }
+    
+    [ObservableProperty]
+    private ObservableCollection<TaskTodo> taskTodoList;
+    
+    [ObservableProperty]
+    private string taskTodoName;
+    
+    [RelayCommand]
+    async Task AddNewTaskTodo()
+    {
+        if (string.IsNullOrEmpty(TaskTodoName))
+            return;
+        if (TaskTodoList.Where(x => x.Name == TaskTodoName).ToList().Count > 0)
+            return;// Mettre un message d'alerte ?
+        
+        var taskTodoToAdd = new TaskTodo()
+        {
+            Name = TaskTodoName,
+            NbrPomodoro = 1
+        };
+        TaskTodoList.Add(taskTodoToAdd);
+        TaskTodoName = string.Empty;
+    }
+    
+    [RelayCommand]
+    void DeleteTaskTodo(int taskTodoIdToDelete)
+    {
+        TaskTodo taskTodoToDelete = TaskTodoList.Where(x => x.Id == taskTodoIdToDelete).ToList().First();
+        
+        if (taskTodoToDelete is not null)
+            TaskTodoList.Remove(taskTodoToDelete);
+    }
+    
+    [RelayCommand]
+    void AddPomodoroToTaskTodo(int taskTodoIdToAddPomodoro)
+    {
+        TaskTodo taskTodoToAddPomodoro = TaskTodoList.Where(x => x.Id == taskTodoIdToAddPomodoro).ToList().First();
+        if (taskTodoToAddPomodoro is not null)
+        {
+            //Todo trouver un meilleur truc pour mettre à jour le compteur de pomodoro
+            int iTaskTodoList = TaskTodoList.IndexOf(taskTodoToAddPomodoro);
+            TaskTodoList.Remove(taskTodoToAddPomodoro); 
+            taskTodoToAddPomodoro.NbrPomodoro++;
+            TaskTodoList.Insert(iTaskTodoList, taskTodoToAddPomodoro);
 
+        }
+    }
+    [RelayCommand]
+    void SubPomodoroToTaskTodo(int taskTodoIdTdSubPomodoro)
+    {
+        TaskTodo taskTodoToSubPomodoro = TaskTodoList.Where(x => x.Id == taskTodoIdTdSubPomodoro).ToList().First();
+        if (taskTodoToSubPomodoro is not null)
+        {
+            //Todo trouver un meilleur truc pour mettre à jour le compteur de pomodoro
+            int iTaskTodoList = TaskTodoList.IndexOf(taskTodoToSubPomodoro);
+            taskTodoToSubPomodoro.NbrPomodoro--;
+            TaskTodoList[iTaskTodoList] = taskTodoToSubPomodoro;
+        }
+    }
+    
+    // [RelayCommand]
+    // async Task Tap(string s)
+    // {
+    //     await Shell.Current.GoToAsync($"{nameof(DetailTaskPage)}?Text={s}");
+    // }
+    
     private void TimerFinished(object sender, EventArgs e)
     {
-        //Todo doit enlever un pomodoro à la tache du dessus et la supprimer si elle ne possède plus de pomodoro
-        Console.WriteLine("J'ai bien recu l'evenement");
-    }
-    [ObservableProperty]
-    private ObservableCollection<string> items;
-    
-    [ObservableProperty]
-    private string text;
-    
-    [RelayCommand]
-    async Task Add()
-    {
-        if (string.IsNullOrEmpty(Text))
+        if(!TaskTodoList.Any())
             return;
-        if (connectivity.NetworkAccess != NetworkAccess.Internet)
-        {
-            await Shell.Current.DisplayAlert("Uh Oh", "No Internet", "OK");
-            return;
-        }
-        Items.Add(Text);
-        Text = string.Empty;
+        SubPomodoroToTaskTodo(TaskTodoList.First().Id);
+        if(TaskTodoList.First().NbrPomodoro <= 0)
+            DeleteTaskTodo(TaskTodoList.First().Id);
     }
+
+
+    #region Old
+    // private IConnectivity connectivity;
+    // public MainViewModel(IConnectivity connectivity)
+    // {
+    //     items = new ObservableCollection<string>();
+    //     this.connectivity = connectivity;
+    // }
     
-    [RelayCommand]
-    void Delete(string s)
-    {
-        if (Items.Contains(s))
-            Items.Remove(s);
-    }
-    
-    [RelayCommand]
-    async Task Tap(string s)
-    {
-        await Shell.Current.GoToAsync($"{nameof(DetailTaskPage)}?Text={s}");
-        //On peut passer des objets complex dans le deuxième paramètre de la méthode dans un dictionnaire
-        // , new Dictionary<string, object>()
-        // {
-        //     nameof(DetailPage), new object()
-        // };
-    }
+    // [ObservableProperty]
+    // private ObservableCollection<string> items;
+    //
+    // [ObservableProperty]
+    // private string text;
+    //
+    // [RelayCommand]
+    // async Task Add()
+    // {
+    //     if (string.IsNullOrEmpty(Text))
+    //         return;
+    //     if (connectivity.NetworkAccess != NetworkAccess.Internet)
+    //     {
+    //         await Shell.Current.DisplayAlert("Uh Oh", "No Internet", "OK");
+    //         return;
+    //     }
+    //     Items.Add(Text);
+    //     Text = string.Empty;
+    // }
+    //
+    // [RelayCommand]
+    // void Delete(string s)
+    // {
+    //     if (Items.Contains(s))
+    //         Items.Remove(s);
+    // }
+    //
+    // [RelayCommand]
+    // async Task Tap(string s)
+    // {
+    //     await Shell.Current.GoToAsync($"{nameof(DetailTaskPage)}?Text={s}");
+    //     //On peut passer des objets complex dans le deuxième paramètre de la méthode dans un dictionnaire
+    //     // , new Dictionary<string, object>()
+    //     // {
+    //     //     nameof(DetailPage), new object()
+    //     // };
+    // }
+    #endregion
+
 }
